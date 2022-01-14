33c5c91ca51d0c7222a6849f3e87d565d1d64a5e
youngseokchung <yschung73@metanet.co.kr>
2022-01-14 05:08:08 +0000

    Added the first tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
@@ -8,14 +8,3 @@ terraform {
     }
   }
 }
-=======
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "youngseokchung-training"
-    workspaces {
-      name = "hashicat-aws"
-    }
-  }
-}
->>>>>>> d2e6e280b3ce999b1813125bcd1ad549335ecf64
